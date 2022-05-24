87e550dc1f59b7de5e3234112606d31647c6665d
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-05-23 22:53:00 -0500

    Add override methods for hashCode, toString, equals

--- src/main/java/com/example/chessbot/model/board/position/BoardPosition.java
+++ src/main/java/com/example/chessbot/model/board/position/BoardPosition.java
@@ -1,5 +1,7 @@
 package com.example.chessbot.model.board.position;
 
+import java.util.Objects;
+
 public class BoardPosition {
     private int x;
     private int y;
@@ -19,11 +21,41 @@ public class BoardPosition {
         return this;
     }
 
-    public int getPositionX() {
+    public int getX() {
         return this.x;
     }
 
-    public int getPositionY() {
+    public int getY() {
         return this.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BoardPosition position = (BoardPosition) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "BoardPosition (" + x + "," + y + ")";
+    }
+
+
+    /**
+     * @param one BoardPosition to compare with instance two
+     * @param two BoardPosition to compare with instance one
+     * @return BoardPosition representing difference on x & y axis between one & two
+     */
+    public static BoardPosition compare(BoardPosition one, BoardPosition two) {
+        int differenceX = one.getX() - two.getX();
+        int differenceY = one.getY() - two.getY();
+        return new BoardPosition(differenceX, differenceY);
     }
 }

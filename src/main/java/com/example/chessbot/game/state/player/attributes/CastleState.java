5dc6a55da6aa4152b187882e6d71cdd4342f9bbb
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-06-03 17:31:13 -0500

    Remove unused classes

--- src/main/java/com/example/chessbot/game/state/player/attributes/CastleState.java
+++
@@ -1,6 +0,0 @@
-package com.example.chessbot.game.state.player.attributes;
-
-public interface CastleState {
-    boolean hasCastled();
-    boolean canCastle();
-}
--- src/main/java/com/example/chessbot/game/validators/movement/MoveValidator.java
+++
@@ -1,8 +0,0 @@
-package com.example.chessbot.game.validators.movement;
-
-import com.example.chessbot.game.state.GameState;
-import com.example.chessbot.model.board.position.BoardPosition;
-
-public interface MoveValidator {
-    boolean validate(GameState gameState, BoardPosition currentPosition, BoardPosition desiredPosition);
-}

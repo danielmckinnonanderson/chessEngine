84196e5d4f9fabe40fa572bcff4e87c64f20b505
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-01 09:41:59 -0500

    Update CheckUtil, WIP

--- src/main/java/com/example/chessbot/game/utility/CheckUtil.java
+++ src/main/java/com/example/chessbot/game/utility/CheckUtil.java
@@ -2,7 +2,7 @@ package com.example.chessbot.game.utility;
 
 import com.example.chessbot.model.state.player.attributes.CheckState;
 import com.example.chessbot.model.board.position.BoardPosition;
-import com.example.chessbot.model.board.position.Pair;
+import com.example.chessbot.model.utility.Pair;
 import com.example.chessbot.model.piece.Piece;
 import com.example.chessbot.model.piece.PieceNames;
 import com.example.chessbot.model.piece.PieceTeam;
@@ -12,15 +12,12 @@ import java.util.*;
 public final class CheckUtil {
 
     public static Pair<CheckState, CheckState> getCheckStatesFromBoard(Map<BoardPosition, Piece> board) {
-        return null;
-    }
-
-    public static CheckState getCheckStateFromBoard(Map<BoardPosition, Piece> board, PieceTeam team) {
         // get location of king for specified team
         Pair<BoardPosition, BoardPosition> kingLocations = getKingLocations(board);
 
         // given location of king, check each opposing piece to see if it has a valid move to capture it currently
         board.forEach((position, piece) -> {
+
         });
         return null;
     }
@@ -38,5 +35,27 @@ public final class CheckUtil {
             // return pair where white king is always on the left
             return new Pair<>(kingLocations.get(0).getLeft(), kingLocations.get(1).getLeft());
         } else return new Pair<>(kingLocations.get(1).getLeft(), kingLocations.get(0).getLeft());
+    }
+
+
+    /**
+     * @param board current pieces on board
+     * @return two lists of pairs, each pair contains a board position that the king is held in check from and the piece present there
+     */
+    public static Pair<List<Pair<BoardPosition, Piece>>, List<Pair<BoardPosition, Piece>>> getListInCheckFrom(Map<BoardPosition, Piece> board, Pair<BoardPosition, BoardPosition> kingLocations) {
+        List<Pair<BoardPosition, Piece>> whiteInCheckFrom = new ArrayList<>();
+        List<Pair<BoardPosition, Piece>> blackInCheckFrom = new ArrayList<>();
+
+        board.forEach((position, piece) -> {
+            if(piece.getPieceTeam() == PieceTeam.WHITE
+                && CaptureUtil.canPutInCheck()) {
+                blackInCheckFrom.add(new Pair(position, piece));
+            } else if (piece.getPieceTeam() == PieceTeam.BLACK
+                && CaptureUtil.canPutInCheck()) {
+                whiteInCheckFrom.add(new Pair(position, piece));
+            }
+        });
+
+        return new Pair<>(whiteInCheckFrom, blackInCheckFrom);
     }
 }

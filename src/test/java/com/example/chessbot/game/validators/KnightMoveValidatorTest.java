8837a543c6aa8bd07509ef6c6c9c01a34981c8c1
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-01 08:48:18 -0500

    Update import for new location of Pair

--- src/test/java/com/example/chessbot/game/validators/KnightMoveValidatorTest.java
+++ src/test/java/com/example/chessbot/game/validators/KnightMoveValidatorTest.java
@@ -7,7 +7,7 @@ import com.example.chessbot.model.state.player.PlayerStateFactory;
 import com.example.chessbot.game.validators.movement.KnightMoveValidator;
 import com.example.chessbot.model.board.BoardFactory;
 import com.example.chessbot.model.board.position.BoardPosition;
-import com.example.chessbot.model.board.position.Pair;
+import com.example.chessbot.model.utility.Pair;
 import com.example.chessbot.model.piece.Piece;
 import com.example.chessbot.model.piece.PieceNames;
 import com.example.chessbot.model.piece.PieceTeam;

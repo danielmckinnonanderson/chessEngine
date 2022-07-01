d26b6fcb59557e47805530ac91d3997464295eb1
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-01 09:42:32 -0500

    Update CaptureUtil

--- src/main/java/com/example/chessbot/game/utility/CaptureUtil.java
+++ src/main/java/com/example/chessbot/game/utility/CaptureUtil.java
@@ -4,8 +4,13 @@ import com.example.chessbot.model.piece.Piece;
 import com.example.chessbot.model.piece.PieceNames;
 
 public final class CaptureUtil {
+
     public static boolean canCapture(Piece self, Piece target) {
         return EnemyPieceUtil.isPieceEnemy(self, target)
                 && target.getPieceName() != PieceNames.KING;
+    }
+
+    public static boolean canCaptureForCheck(Piece self, Piece target) {
+        return EnemyPieceUtil.isPieceEnemy(self, target);
     }
 }

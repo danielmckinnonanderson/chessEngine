4074a0327093ec2148235e0943cf0ace1cd084ce
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-01 08:35:19 -0500

    Move class Pair

---
+++ src/main/java/com/example/chessbot/model/utility/Pair.java
@@ -0,0 +1,34 @@
+package com.example.chessbot.model.board.position;
+
+import java.util.Objects;
+
+public class Pair<L, R> {
+    private final L left;
+    private final R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return left.equals(pair.left) && right.equals(pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}

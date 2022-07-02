40cf1a513bbd7aecea0ddb1cdd155bd0cf5e61e6
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-02 00:06:34 -0500

    Add more unit tests

--- src/test/java/com/example/chessbot/engine/ValidMoveGeneratorTest.java
+++ src/test/java/com/example/chessbot/engine/ValidMoveGeneratorTest.java
@@ -15,11 +15,11 @@ import java.util.Map;
 public class ValidMoveGeneratorTest {
     static final Piece whitePawn = new Piece(PieceNames.PAWN, PieceTeam.WHITE, true);
     static final Piece blackPawn = new Piece(PieceNames.PAWN, PieceTeam.BLACK, true);
-    static final Piece rook = new Piece(PieceNames.PAWN, PieceTeam.WHITE, true);
-    static final Piece knight = new Piece(PieceNames.PAWN, PieceTeam.BLACK, true);
-    static final Piece bishop = new Piece(PieceNames.PAWN, PieceTeam.WHITE, true);
-    static final Piece queen = new Piece(PieceNames.PAWN, PieceTeam.BLACK, true);
-    static final Piece king = new Piece(PieceNames.PAWN, PieceTeam.WHITE, true);
+    static final Piece rook = new Piece(PieceNames.ROOK, PieceTeam.WHITE, true);
+    static final Piece knight = new Piece(PieceNames.KNIGHT, PieceTeam.BLACK, true);
+    static final Piece bishop = new Piece(PieceNames.BISHOP, PieceTeam.WHITE, true);
+    static final Piece queen = new Piece(PieceNames.QUEEN, PieceTeam.BLACK, true);
+    static final Piece king = new Piece(PieceNames.KING, PieceTeam.WHITE, true);
 
     static Map<BoardPosition, Piece> board;
 
@@ -29,19 +29,65 @@ public class ValidMoveGeneratorTest {
     }
 
     @Test
-    public void testGeneratePawnPath() {
+    public void testGenerateWhitePawnPath() {
         int x = 4;
         int y = 2;
-        final BoardPosition pos = new BoardPosition(4, 2);
+        final BoardPosition pos = new BoardPosition(x, y);
 
         board.put(pos, whitePawn);
 
-        final List<BoardPosition> result = ValidMoveGenerator.generateListOfValidMoves(whitePawn, pos, board);
+        final List<BoardPosition> result = ValidMoveGenerator.listOfPotentialMovesFromPieceName(whitePawn, pos);
 
         Assertions.assertThat(result.contains(new BoardPosition(x + 1, y + 1))).isTrue();
         Assertions.assertThat(result.contains(new BoardPosition(x, y + 2))).isTrue();
         Assertions.assertThat(result.contains(new BoardPosition(x, y + 1))).isTrue();
         Assertions.assertThat(result.contains(new BoardPosition(x - 1, y + 1))).isTrue();
         Assertions.assertThat(result.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void testGenerateBlackPawnPath() {
+        int x = 7;
+        int y = 4;
+        final BoardPosition pos = new BoardPosition(x, y);
+
+        board.put(pos, whitePawn);
+
+        final List<BoardPosition> result = ValidMoveGenerator.listOfPotentialMovesFromPieceName(blackPawn, pos);
+
+        Assertions.assertThat(result.contains(new BoardPosition(x + 1, y - 1))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y - 2))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y - 1))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x - 1, y -1))).isTrue();
+        Assertions.assertThat(result.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void testGenerateRookPath() {
+        int x = 1;
+        int y = 1;
+        final BoardPosition pos = new BoardPosition(x, y);
+
+        board.put(pos, rook);
+
+        final List<BoardPosition> result = ValidMoveGenerator.listOfPotentialMovesFromPieceName(rook, pos);
+
+        Assertions.assertThat(result.contains(new BoardPosition(x + 1, y))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x + 2, y))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x + 3, y))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x + 4, y))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x + 5, y))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x + 6, y))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x + 7, y))).isTrue();
+
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 1))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 2))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 3))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 4))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 5))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 6))).isTrue();
+        Assertions.assertThat(result.contains(new BoardPosition(x, y + 7))).isTrue();
+
+        Assertions.assertThat(result.size()).isEqualTo(14);
     }
 }

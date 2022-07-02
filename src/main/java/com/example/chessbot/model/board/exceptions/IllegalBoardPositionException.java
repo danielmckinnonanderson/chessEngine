a7ff21637033d03990cb5f5e02cf86b1dadbdeab
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-01 20:11:48 -0500

    Remove unused class

--- src/main/java/com/example/chessbot/model/board/exceptions/IllegalBoardPositionException.java
+++
@@ -1,7 +0,0 @@
-package com.example.chessbot.model.board.exceptions;
-
-public class IllegalBoardPositionException extends Exception {
-    public IllegalBoardPositionException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-}

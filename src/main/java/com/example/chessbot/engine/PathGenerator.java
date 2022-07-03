aa4c4fbb67aa18cbf081bea690c7b577f3f10e27
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2022-07-03 15:18:47 -0500

    Update PathGenerator

--- src/main/java/com/example/chessbot/engine/PathGenerator.java
+++ src/main/java/com/example/chessbot/engine/PathGenerator.java
@@ -8,26 +8,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class PathGenerator {
+public final class PathGenerator {
     public static List<BoardPosition> generatePossiblePawnPaths(PieceTeam team, BoardPosition origin) {
         int x = origin.getX();
         int y = origin.getY();
 
-        if(team == PieceTeam.WHITE) {
-            return List.of(
-                    new BoardPosition(x, y+1),
-                    new BoardPosition(x, y+2),
-                    new BoardPosition(x+1, y+1),
-                    new BoardPosition(x-1, y+1)
-            );
+        if (team == PieceTeam.WHITE) {
+            return Stream.of(
+                            new BoardPosition(x, y + 1),
+                            new BoardPosition(x, y + 2),
+                            new BoardPosition(x + 1, y + 1),
+                            new BoardPosition(x - 1, y + 1)
+                    ).filter((boardPosition -> (boardPosition.getX() >= 1 && boardPosition.getX() <= 8
+                            && boardPosition.getY() >= 1 && boardPosition.getY() <= 8)))
+                    .collect(Collectors.toList());
         }
-        if(team == PieceTeam.BLACK) {
-            return List.of(
-                    new BoardPosition(x, y-1),
-                    new BoardPosition(x, y-2),
-                    new BoardPosition(x+1, y-1),
-                    new BoardPosition(x-1, y-1)
-            );
+        if (team == PieceTeam.BLACK) {
+            return Stream.of(
+                            new BoardPosition(x, y - 1),
+                            new BoardPosition(x, y - 2),
+                            new BoardPosition(x + 1, y - 1),
+                            new BoardPosition(x - 1, y - 1)
+                    ).filter((boardPosition -> (boardPosition.getX() >= 1 && boardPosition.getX() <= 8
+                            && boardPosition.getY() >= 1 && boardPosition.getY() <= 8)))
+                    .collect(Collectors.toList());
         }
         return List.of();
     }
@@ -35,10 +39,10 @@ public class PathGenerator {
     // TODO probably don't need to make a bunch of streams in any of these. Refactor later
     public static List<BoardPosition> generatePossibleRookPaths(BoardPosition origin) {
         return Stream.of(
-                generateUpPath(origin),
-                generateRightPath(origin),
-                generateDownPath(origin),
-                generateLeftPath(origin))
+                        generateUpPath(origin),
+                        generateRightPath(origin),
+                        generateDownPath(origin),
+                        generateLeftPath(origin))
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
     }
@@ -48,29 +52,31 @@ public class PathGenerator {
         int y = origin.getY();
 
         return Stream.of(
-            new BoardPosition(x + 1, y + 2),
-            new BoardPosition(x + 1, y - 2),
-            new BoardPosition(x - 1, y + 2),
-            new BoardPosition(x - 1, y - 2),
-            new BoardPosition(x + 2, y + 1),
-            new BoardPosition(x + 2, y - 1),
-            new BoardPosition(x - 2, y + 1),
-            new BoardPosition(x - 2, y - 1)
-        ).filter((boardPosition ->
-                boardPosition.getY() >= 1 && boardPosition.getY() <= 8
-                && boardPosition.getX() >= 1 && boardPosition.getX() <=8 ))
+                        new BoardPosition(x + 1, y + 2),
+                        new BoardPosition(x + 1, y - 2),
+                        new BoardPosition(x - 1, y + 2),
+                        new BoardPosition(x - 1, y - 2),
+                        new BoardPosition(x + 2, y + 1),
+                        new BoardPosition(x + 2, y - 1),
+                        new BoardPosition(x - 2, y + 1),
+                        new BoardPosition(x - 2, y - 1)
+                ).filter((boardPosition ->
+                        boardPosition.getY() >= 1 && boardPosition.getY() <= 8
+                                && boardPosition.getX() >= 1 && boardPosition.getX() <= 8))
                 .collect(Collectors.toList());
     }
 
+    //TODO could probably make this a wee bit more efficient if I passed in the X and Y values
+    //   instead of getting X and Y from the origin at the beginning of each func every time
     public static List<BoardPosition> generatePossibleBishopPaths(BoardPosition origin) {
         int x = origin.getX();
         int y = origin.getY();
 
         return Stream.of(
-                generateUpRightPath(origin),
-                generateDownRightPath(origin),
-                generateUpLeftPath(origin),
-                generateDownLeftPath(origin))
+                        generateUpRightPath(origin),
+                        generateDownRightPath(origin),
+                        generateDownLeftPath(origin),
+                        generateUpLeftPath(origin))
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
     }
@@ -80,14 +86,14 @@ public class PathGenerator {
         int y = origin.getY();
 
         return Stream.of(
-                generateUpPath(origin),
-                generateUpRightPath(origin),
-                generateRightPath(origin),
-                generateDownRightPath(origin),
-                generateDownPath(origin),
-                generateDownLeftPath(origin),
-                generateLeftPath(origin),
-                generateUpLeftPath(origin))
+                        generateUpPath(origin),
+                        generateUpRightPath(origin),
+                        generateRightPath(origin),
+                        generateDownRightPath(origin),
+                        generateDownPath(origin),
+                        generateDownLeftPath(origin),
+                        generateLeftPath(origin),
+                        generateUpLeftPath(origin))
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
     }
@@ -96,15 +102,18 @@ public class PathGenerator {
         int x = origin.getX();
         int y = origin.getY();
 
-        return List.of(
-                new BoardPosition(x, y + 1),
-                new BoardPosition(x + 1, y + 1),
-                new BoardPosition(x + 1, y),
-                new BoardPosition(x + 1, y - 1),
-                new BoardPosition(x, y - 1),
-                new BoardPosition(x - 1, y - 1),
-                new BoardPosition(x - 1, y),
-                new BoardPosition(x - 1, y + 1));
+        return Stream.of(
+                        new BoardPosition(x, y + 1),
+                        new BoardPosition(x + 1, y + 1),
+                        new BoardPosition(x + 1, y),
+                        new BoardPosition(x + 1, y - 1),
+                        new BoardPosition(x, y - 1),
+                        new BoardPosition(x - 1, y - 1),
+                        new BoardPosition(x - 1, y),
+                        new BoardPosition(x - 1, y + 1))
+                .filter((boardPosition -> (boardPosition.getX() >= 1 && boardPosition.getX() <= 8
+                        && boardPosition.getY() >= 1 && boardPosition.getY() <= 8)))
+                .collect(Collectors.toList());
     }
 
 
@@ -119,8 +128,8 @@ public class PathGenerator {
 
         List<BoardPosition> result = new ArrayList<>();
 
-        for(int i = originY + 1; i <= spacesToEdge + 1; i++) {
-            result.add(new BoardPosition(originX, i));
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX, originY + i));
         }
 
         return result;
@@ -131,14 +140,14 @@ public class PathGenerator {
         int originX = origin.getX();
         int spacesToEdge = 8 - originX < 8 - origin.getY() ? 8 - originX : 8 - originY;
 
-        if(spacesToEdge == 0) {
+        if (spacesToEdge == 0) {
             return List.of();
         }
 
         List<BoardPosition> result = new ArrayList<>();
 
-        for(int i = 0; i < spacesToEdge; i++) {
-            result.add(new BoardPosition(originX + 1, originY + 1));
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX + i, originY + i));
         }
 
         return result;
@@ -150,14 +159,14 @@ public class PathGenerator {
         int originX = origin.getX();
         int spacesToEdge = 8 - originX;
 
-        if(spacesToEdge == 0) {
+        if (spacesToEdge == 0) {
             return List.of();
         }
 
         List<BoardPosition> result = new ArrayList<>();
 
-        for(int i = originX + 1; i <= spacesToEdge + 1; i++) {
-            result.add(new BoardPosition(i, originY));
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX + i, originY));
         }
 
         return result;
@@ -174,8 +183,8 @@ public class PathGenerator {
 
         List<BoardPosition> result = new ArrayList<>();
 
-        for(int i = originX; i <= spacesToEdge; i++) {
-            result.add(new BoardPosition(i, originY));
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX + i, originY - i));
         }
 
         return result;
@@ -186,21 +195,35 @@ public class PathGenerator {
         int originX = origin.getX();
         int spacesToEdge = originY - 1;
 
-        if(spacesToEdge == 0 ) {
+        if (spacesToEdge == 0) {
             return List.of();
         }
 
         List<BoardPosition> result = new ArrayList<>();
 
-        for(int i = originY - 1; i >= spacesToEdge - 1; i--) {
-            result.add(new BoardPosition(originX, i));
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX, originY - i));
         }
 
         return result;
     }
 
     public static List<BoardPosition> generateDownLeftPath(BoardPosition origin) {
-        return List.of();
+        int originY = origin.getY();
+        int originX = origin.getX();
+        int spacesToEdge = 8 - originX < origin.getY() - 1 ? 8 - originX : originY - 1;
+
+        if (spacesToEdge == 0) {
+            return List.of();
+        }
+
+        List<BoardPosition> result = new ArrayList<>();
+
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX - i, originY - i));
+        }
+
+        return result;
     }
 
     public static List<BoardPosition> generateLeftPath(BoardPosition origin) {
@@ -208,20 +231,34 @@ public class PathGenerator {
         int originX = origin.getX();
         int spacesToEdge = originX - 1;
 
-        if(spacesToEdge == 0) {
+        if (spacesToEdge == 0) {
             return List.of();
         }
 
         List<BoardPosition> result = new ArrayList<>();
 
-        for(int i = originY; i >= spacesToEdge; i--) {
-            result.add(new BoardPosition(originX, i));
+        for (int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX - i, originY));
         }
 
         return result;
     }
 
     public static List<BoardPosition> generateUpLeftPath(BoardPosition origin) {
-        return List.of();
+        int originY = origin.getY();
+        int originX = origin.getX();
+        int spacesToEdge = 8 - originX < origin.getY() - 1 ? 8 - originX : originY - 1;
+
+        if(spacesToEdge == 0) {
+            return List.of();
+        }
+
+        List<BoardPosition> result = new ArrayList<>();
+
+        for(int i = 1; i <= spacesToEdge; i++) {
+            result.add(new BoardPosition(originX - i, originY + i));
+        }
+
+        return result;
     }
 }
